b6f500adf9f3b7fb59f5de559d9380e2ee11e008
minjee2758 <minjee2758@naver.com>
2025-03-26 15:18:56 +0900

    [refactor] 예외처리 추가

--- src/main/java/com/example/todoapp/controller/TodoController.java
+++ src/main/java/com/example/todoapp/controller/TodoController.java
@@ -22,18 +22,21 @@ public class TodoController {
     }
 
     @PostMapping //투두 추가하는거니까 Post
-    public ResponseDto saveTodo(@RequestBody RequestDto dto){ //추가하라고 요청하기
-        return todoService.saveTodo(dto);
+    public ResponseEntity<ResponseDto> saveTodo(@RequestBody RequestDto dto){ //추가하라고 요청하기
+        ResponseDto responseDto = todoService.saveTodo(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
     }
 
     @GetMapping //전체 일정 가져오기
-    public List<ResponseDto> findAllTodo(){
-        return todoService.findAllTodo();
+    public ResponseEntity<List<ResponseDto>> findAllTodo(){
+        List<ResponseDto> responseDtoList = todoService.findAllTodo();
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(responseDtoList);
     }
 
     @GetMapping("/{id}") //선택 일정 가져오기
-    public ResponseDto findTodoById(@PathVariable Long id){
-        return todoService.findTodoById(id);
+    public ResponseEntity<ResponseDto> findTodoById(@PathVariable Long id){
+        ResponseDto responseDto = todoService.findTodoById(id);
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(responseDto);
     }
 
     //수정
